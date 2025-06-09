b396c32b82cae0a663c24abf1ee34fba813c6ed3
nishanth <nishanthac1611@gmail.com>
2025-06-09 15:45:04 +0530

    added

---
+++ javascript/add.py
@@ -0,0 +1,21 @@
+a=30
+b=60
+print(b-a)
+print(type(a))
+c="string"
+d = True
+pi=3.14
+print(type(c))
+print(type(d))
+print(type(pi))
+
+list = [1,2,3,4,5,6,7];
+for i in list:
+    print(i)
+print(type(list))
+
+dictionary={"name":"karhti","passenger":"123456"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
